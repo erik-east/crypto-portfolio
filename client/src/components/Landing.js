050063a7d5190d55e1729f85a2ba8ccde6af1581
Erk Eyupgiller <erk.eyupgiller@gmail.com>
2018-03-30 17:02:51 +0300

    Initialize create-react-app, write boilerplate code for react/redux/react-router/redux-thunk and integrate front end and back end with authentication features

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center'}}>
+            <h1>
+                Product Logo
+            </h1>
+            Tell your visitors what is the product for here!
+        </div>
+    );
+};
+
+export default Landing;
