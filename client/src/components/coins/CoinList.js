b8b546bcf68a3ed4bd0a1d68e9ecd1aedca6c55b
Erk Eyupgiller <erk.eyupgiller@gmail.com>
2018-04-01 14:25:56 +0300

    fix variable type of percentage changes

--- client/src/components/coins/CoinList.js
+++ client/src/components/coins/CoinList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import _ from 'lodash';
 import { connect } from 'react-redux';
 import { fetchCoinList } from '../../actions';
 
@@ -17,9 +16,9 @@ class CoinList extends Component {
                     <td className="center-align">{coin.symbol}</td>
                     <td className="right-align">{coin.price_usd} $</td>
                     <td className="right-align">{coin.price_btc} BTC</td>
-                    <td className="center-align" style={_.includes(coin.percent_change_1h, '-') ? {color: '#ef0404'} : {color: '#56db0f'}}>{coin.percent_change_1h}%</td>
-                    <td className="center-align" style={_.includes(coin.percent_change_24h, '-') ? {color: '#ef0404'} : {color: '#56db0f'}}>{coin.percent_change_24h}%</td>
-                    <td className="center-align" style={_.includes(coin.percent_change_7d, '-') ? {color: '#ef0404'} : {color: '#56db0f'}}>{coin.percent_change_7d}%</td>
+                    <td className="center-align" style={coin.percent_change_1h < 0 ? {color: '#ef0404'} : {color: '#56db0f'}}>{coin.percent_change_1h}%</td>
+                    <td className="center-align" style={coin.percent_change_24h < 0 ? {color: '#ef0404'} : {color: '#56db0f'}}>{coin.percent_change_24h}%</td>
+                    <td className="center-align" style={coin.percent_change_7d < 0 ? {color: '#ef0404'} : {color: '#56db0f'}}>{coin.percent_change_7d}%</td>
                     <td>{moment.unix(coin.last_updated).format('d/MM/YYYY HH:mm')}</td>
                 </tr>
             )
