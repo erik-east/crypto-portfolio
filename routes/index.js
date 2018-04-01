2b36e66722c421712cd89a7413dd2d29d2193e07
Raif Emre Akbay <raif.akbay@getir.com>
2018-04-01 13:59:03 +0300

    reorder

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,6 @@
 
 module.exports = app => {
 
-  if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets
-    // like our main.js file, or main.css file!
-    app.use(express.static('client/build'));
-
-    // Express will serve up the index.html file
-    // if it doesn't recognize the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-
   // subroutes
   app.use('/auth',require('./authRoutes'));
   app.use('/api/coin',require('./coinRoutes'));
@@ -28,4 +15,17 @@ module.exports = app => {
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
+
+  if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 };
