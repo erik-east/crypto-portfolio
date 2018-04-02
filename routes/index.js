0166839b4267e808b7d60f0a99b2a80a711063a7
Erk Eyupgiller <erk.eyupgiller@gmail.com>
2018-04-02 17:05:34 +0300

    hotfix-3

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-const express = require('express');
 module.exports = app => {
 
   // subroutes
@@ -14,17 +13,4 @@ module.exports = app => {
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
-
-  if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets
-    // like our main.js file, or main.css file!
-    app.use(express.static('client/build'));
-
-    // Express will serve up the index.html file
-    // if it doesn't recognize the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
 };
